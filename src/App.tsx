bdf55f8df3c5f61ed6ba9179d1f83c4e53af9c3b
David Rose <doppler@gmail.com>
2021-10-25 19:43:14 -0400

    add dr-logo and spiralstar, begin site design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import './App.css';
-
+import DRLogo from './dr-logo.svg';
 function App() {
-  return <div className="App">@dopplerreflect</div>;
+  return (
+    <div className="App">
+      <div className="Header">
+        <img src={DRLogo} />
+        dopplerreflect.art
+      </div>
+    </div>
+  );
 }
 
 export default App;
