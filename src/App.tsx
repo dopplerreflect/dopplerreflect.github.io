5760d097d4f9949093961bcf2f862c3519b01401
David Rose <doppler@gmail.com>
2021-10-25 20:22:24 -0400

    add body and mv cname to public so it'll get copied to docs

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,25 @@ import DRLogo from './dr-logo.svg';
 function App() {
   return (
     <div className="App">
-      <div className="Header">
+      <header>
         <img src={DRLogo} />
         dopplerreflect.art
-      </div>
+      </header>
+      <main>
+        <h1>About</h1>
+        <p>
+          Hello, I'm <strong>doppler</strong>, aka David Rose.
+        </p>
+        <p>
+          When I'm not teaching people to skydive or filming someone's first
+          tandem skydive, I enjoy creating visual art with code.
+        </p>
+        <p>
+          This website will serve as a showcase for some of my work. In the
+          future, I hope to share the work via physical media such as on
+          jerseys, canvas, decals, etc.
+        </p>
+      </main>
     </div>
   );
 }
