160277feb0a7d1d53788ce520c4252227a127ca0
David Rose <doppler@gmail.com>
2021-10-27 21:02:41 -0400

    add social icons

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import DRLogo from './dr-logo.svg';
 import CloseButton from './close-button.svg';
 import Title from './title-filter.svg';
 import { images, ImageDetail } from './images/index';
-
+import { SocialIcon } from 'react-social-icons';
 const sanitize = (s: string): string => s.replace(/[^\w]/, '').toLowerCase();
 
 const hideImageDetails = (): void =>
@@ -31,27 +31,62 @@ function App() {
         <img id="title" src={Title} />
       </header>
       <main>
-        <div id="Gallery">
+        <section id="Gallery">
           {images.map(image => (
             <div className="Image" key={image.src}>
               <img src={image.src} onClick={() => displayImageDetails(image)} />
               <ImageDetailCard image={image} />
             </div>
           ))}
-        </div>
-        <h1>About</h1>
-        <p>
-          Hello, I'm <strong>doppler</strong>, aka David Rose.
-        </p>
-        <p>
-          When I'm not teaching people to skydive or filming someone's first
-          tandem skydive, I enjoy creating visual art with code.
-        </p>
-        <p>
-          This website will serve as a showcase for some of my work. In the
-          future, I hope to share the work via physical media such as on
-          jerseys, canvas, decals, etc.
-        </p>
+        </section>
+        <section id="About">
+          <h1>About</h1>
+          <p>
+            Hello, I'm <strong>doppler</strong>, aka David Rose.
+          </p>
+          <p>
+            When I'm not teaching people to skydive or filming someone's first
+            tandem skydive, I enjoy creating visual art with code.
+          </p>
+          <p>
+            This website will serve as a showcase for some of my work. In the
+            future, I hope to share the work via physical media such as on
+            jerseys, canvas, decals, etc.
+          </p>
+        </section>
+        <section id="Connect">
+          <h1>Connect</h1>
+          <SocialIcon
+            target="_blank"
+            bgColor="hsl(225, 100%, 20%)"
+            fgColor="hsl(45, 100%, 50%)"
+            url="https://facebook.com/doppler"
+          />
+          <SocialIcon
+            target="_blank"
+            bgColor="hsl(225, 100%, 20%)"
+            fgColor="hsl(45, 100%, 50%)"
+            url="https://twitter.com/dopplerreflect"
+          />
+          <SocialIcon
+            target="_blank"
+            bgColor="hsl(225, 100%, 20%)"
+            fgColor="hsl(45, 100%, 50%)"
+            url="https://github.com/dopplerreflect/"
+          />
+          <SocialIcon
+            target="_blank"
+            bgColor="hsl(225, 100%, 20%)"
+            fgColor="hsl(45, 100%, 50%)"
+            url="https://www.instagram.com/dopplerreflect/"
+          />
+          <SocialIcon
+            target="_blank"
+            bgColor="hsl(225, 100%, 20%)"
+            fgColor="hsl(45, 100%, 50%)"
+            url="mailto:doppler@gmail.com"
+          />
+        </section>
       </main>
     </div>
   );
