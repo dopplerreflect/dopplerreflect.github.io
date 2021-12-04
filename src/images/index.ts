64a3135ed41f1baabb1b81f31631e09dab4eb0dc
David Rose <doppler@gmail.com>
2021-12-04 09:28:40 -0500

    add FlowerOfLifeWithCode and StainedGlass

--- src/images/index.ts
+++ src/images/index.ts
@@ -9,9 +9,11 @@ import StarMandala from './StarMandala.svg';
 import BlueprintStarMandala from './BlueprintStarMandala.svg';
 import FlowerOfLife from './FlowerOfLife.svg';
 import FlowerOfLife2 from './FlowerOfLife2.svg';
+import FlowerOfLifeWithCode from './FlowerOfLifeWithCode.png';
 import HexSunflower from './HexSunflower.svg';
 import Metatron from './Metatron.svg';
 import Tesseract from './Tesseract.svg';
+import StainedGlass from './StainedGlass.png';
 
 import Title from '../title-filter-e.svg';
 
@@ -29,9 +31,24 @@ const images: ImageList = [
     desc: 'Stylized D and R constructed on a pentagram.',
   },
   {
-    src: FlowerOfLife2,
-    title: 'Flower of Life 2',
-    desc: 'A complete rewrite of the previous Flower of Life drawing, with better masking for transparency.',
+    src: SpiralStar,
+    title: 'SpiralStar',
+    desc: 'Lots of stars. And the golden ratio. And spirals. And gradients. This is the basis for a small batch of jerseys I have on order.',
+  },
+  {
+    src: BlueprintStarMandala,
+    title: 'Blueprint',
+    desc: 'Mandala built on a blueprint of rings spaced on golden divisions.',
+  },
+  {
+    src: Metatron,
+    title: "Metatron's Cube",
+    desc: 'A sacred geometry figure.',
+  },
+  {
+    src: StainedGlass,
+    title: 'Stained Glass',
+    desc: 'Translucent image simulating stained glass.',
   },
   {
     src: StarMandala,
@@ -44,9 +61,29 @@ const images: ImageList = [
     desc: 'From Wikipedia: "In geometry, an epicycloid is a plane curve produced by tracing the path of a chosen point on the circumference of a circle—called an epicycle—which rolls without slipping around a fixed circle"',
   },
   {
-    src: Metatron,
-    title: "Metatron's Cube",
-    desc: 'A sacred geometry figure.',
+    src: HexSunflower,
+    title: 'Hexagon Sunflower',
+    desc: 'Sunflower pattern made with hexagons',
+  },
+  {
+    src: FlowerOfLife,
+    title: 'Flower of Life',
+    desc: 'Flower of Life mandala.',
+  },
+  {
+    src: FlowerOfLife2,
+    title: 'Flower of Life 2',
+    desc: 'A complete rewrite of the previous Flower of Life drawing, with better masking for transparency.',
+  },
+  {
+    src: FlowerOfLifeWithCode,
+    title: 'Flowe of Life With Code',
+    desc: 'Flower of Life, with a screenshot of some of the code that generated it as a background.',
+  },
+  {
+    src: CornerReflector,
+    title: 'Corner Reflector',
+    desc: 'Divide the sides of square into golden sections and bounce a line from a corner off of it, and add some translucent colors.',
   },
   {
     src: Tesseract,
@@ -59,11 +96,6 @@ const images: ImageList = [
     desc: 'Colored dots arranged in a <a href="https://en.wikipedia.org/wiki/Cardioid" target="_blank">Cardioid</a> pattern.',
   },
   {
-    src: HexSunflower,
-    title: 'Hexagon Sunflower',
-    desc: 'Sunflower pattern made with hexagons',
-  },
-  {
     src: MaurerRose,
     title: 'Maurer Rose',
     desc: `I don't really know a way to succinctly explain this. See the Wikipedia article for <a href="https://en.wikipedia.org/wiki/Maurer_rose" target="_blank">Maurer Rose</a>.`,
@@ -72,26 +104,6 @@ const images: ImageList = [
     src: Elliptical,
     title: 'Elliptical',
     desc: `Choose a point within (or outiside of) a circle and draw rays from that point to the edge of the circle. Rotate each ray at its midpoint 90 degrees. The resulting lines create overlap to create an ellipse with the circle's midpoint and the chosen point as its focal points. This is that, five times.`,
-  },
-  {
-    src: SpiralStar,
-    title: 'SpiralStar',
-    desc: 'Lots of stars. And the golden ratio. And spirals. And gradients. This is the basis for a small batch of jerseys I have on order.',
-  },
-  {
-    src: BlueprintStarMandala,
-    title: 'Blueprint',
-    desc: 'Mandala built on a blueprint of rings spaced on golden divisions.',
-  },
-  {
-    src: FlowerOfLife,
-    title: 'Flower of Life',
-    desc: 'Flower of Life mandala.',
-  },
-  {
-    src: CornerReflector,
-    title: 'Corner Reflector',
-    desc: 'Divide the sides of square into golden sections and bounce a line from a corner off of it, and add some translucent colors.',
   },
   {
     src: Title,
